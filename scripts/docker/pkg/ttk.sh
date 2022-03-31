a378b8f75980c4aa84f87e44aac8dc0cebadbad9
floWetzels <f_wetzels13@cs.uni-kl.de>
2022-03-31 13:57:31 +0200

    some changes

--- scripts/docker/pkg/ttk.sh
+++ scripts/docker/pkg/ttk.sh
@@ -42,29 +42,6 @@ fi
 (curl -kL "https://github.com/topology-tool-kit/ttk/archive/v${TTK_VERSION}.tar.gz" | tar zx --strip-components 1)
 
 # actually compile
-if [ "${DEV}"=="0.9.9" ]; then
-        mkdir build
-        pushd build
-        
-        cmake -G Ninja \
-            -DTTK_BUILD_DOCUMENTATION=OFF \
-            -DTTK_BUILD_PARAVIEW_PLUGINS=ON \
-            -DTTK_BUILD_STANDALONE_APPS=OFF \
-            -DTTK_BUILD_VTK_WRAPPERS=ON \
-            -DTTK_BUILD_VTK_PYTHON_MODULE=OFF \
-            -DTTK_ENABLE_DOUBLE_TEMPLATING=OFF \
-            -DTTK_ENABLE_CPU_OPTIMIZATION=OFF \
-            -DTTK_ENABLE_OPENMP=ON \
-            -DTTK_ENABLE_KAMIKAZE=ON \
-            ..
-
-        # call Ninja manually to ignore duplicate targets
-        ninja -w dupbuild=warn install 
-        cmake --install .
-        
-        popd
-        exit
-fi
 cmake-default \
     -DTTK_BUILD_DOCUMENTATION=OFF \
     -DTTK_BUILD_PARAVIEW_PLUGINS=ON \
